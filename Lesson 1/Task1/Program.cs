a5c6a48a3c761386b844315db3102b97938f6f8e
Arsen Aliev <thlmovie@gmail.com>
2024-02-14 23:08:03 +0300

    added few variables

--- Lesson 1/Task1/Program.cs
+++ Lesson 1/Task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+ int a = 115;
+Console.WriteLine(a);
+
+double d = 5.55;
+char c = '!';
+Boolean b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
+
