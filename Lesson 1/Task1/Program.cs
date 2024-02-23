04cf5540ede4efffcfa11be20251fc7f52f4aba3
Arsen Aliev <thlmovie@gmail.com>
2024-02-23 03:35:48 +0300

    ArraysHomework

--- Lesson 1/Task1/Program.cs
+++ Lesson 1/Task1/Program.cs
@@ -1,20 +1,197 @@
-﻿//Console.WriteLine("Hello, World!");
+﻿// //Console.WriteLine("Hello, World!");
 
- int a = 115;
-Console.WriteLine(a);
+//  int a = 115;
+// Console.WriteLine(a);
 
-double d = 5.55;
-char c = '!';
-Boolean b = true;
-Console.WriteLine(d);
-Console.WriteLine(c);
-Console.WriteLine(b);
+// double d = 5.55;
+// char c = '!';
+// Boolean b = true;
+// Console.WriteLine(d);
+// Console.WriteLine(c);
+// Console.WriteLine(b);
 
 
 
-int[] arr = new int[5];
-arr[0] = 5;
-arr[1] = 544;
-arr[2] = 13;
-arr[3] = 525;
-arr[4] = 515;
+// int[] arr = new int[5];
+// arr[0] = 5;
+// arr[1] = 544;
+// arr[2] = 13;
+// arr[3] = 525;
+// arr[4] = 515;
+
+
+// // Задайте массив из 10 элементов, заполненный числами из
+// // промежутка [-10, 10]. Замените отрицательные элементы на
+// // положительные, а положительные на отрицательные.
+
+
+// int [] array = {1,-9,7,4,-5,9,-3,-4,-2,2};
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = array[i] * -1;
+//     Console.Write(array[i] + "," + " ");
+// }
+
+// // Найдите произведения пар чисел в одномерном массиве. Парой
+// // считаем первый и последний элемент, второй и предпоследний и
+// // т.д. Результат запишите в новый массив.
+// Console.WriteLine();
+
+// Console.WriteLine("*******************");
+
+// int [] numbers2 = {1,2,3,4,5,6,7};
+// int [] numbers3 = new int[numbers2.Length/2];
+// int count = 1;
+// int multiply;
+// for (int i = 0; i < numbers2.Length/2; i++)
+// {
+//     multiply = numbers2[i]*numbers2[numbers2.Length-count];
+//     numbers3[i] = multiply;
+//     count ++;
+// }
+// foreach (int item in numbers3)
+// {
+//     Console.Write(item);
+//     Console.Write(" ");
+// }
+
+
+
+
+
+// //Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+// class UserInputToCompileForTest
+// { 
+  
+//     // Подсчет количества элементов массива, попадающих в заданный диапазон
+//     // numbers - массив, в котором ведется подсчет
+//     // minRange - минимальная граница диапазона
+//     // maxRange - максимальная граница диапазона
+//     public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
+//     {
+//         int count = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// { 
+//     if (numbers[i]>=minRange & numbers[i] <= maxRange){
+//         count++;
+//     }
+// }
+//        return count;
+//     }
+    
+//     public static void PrintResult(int[] array)
+//     { int count = 0;
+        
+//          for (int i = 0; i < array.Length; i++)
+// { 
+//     if (array[i]>=10 & array[i] <= 90){
+//         count++;
+//     }
+
+// }
+// Console.Write(count);
+//     }
+
+// }
+
+
+// //Не удаляйте и не меняйте класс Answer!
+// class Answer
+// {
+//     public static void Main(string[] args)
+//     {
+//         int[] array;
+        
+
+//         if (args.Length >= 1) {
+//             // Объединяем все аргументы командной строки в одну строку
+//             string joinedArgs = string.Join(" ", args);
+
+//             // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+//             array = joinedArgs.Split(", ")
+//                                   .Select(int.Parse)
+//                                   .ToArray();
+            
+//             // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+//         } else {
+//            // Если аргументов на входе нет
+//             array = new int[] {1, 5, 10, 20, 30, 40, 99, 4, 90, 3}; // Создание массива
+//         }
+//         UserInputToCompileForTest.PrintResult(array);
+//     }
+//
+//}
+
+class UserInputToCompileForTest
+{ 
+    // Подсчет количества четных элементов массива
+    // numbers - массив, в котором ведется подсчет
+    public static int CountEvenItems(int[] numbers)
+    {
+        int count = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i]%2==0){
+                count++;
+            }
+        }
+       return count;
+    }
+    
+    public static void PrintResult(int[] array)
+    {
+      int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i]%2==0){
+                count++;
+            }
+            
+        }
+        Console.Write(count);
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new int[] {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Lesson 1/Task2/Program.cs
+++ Lesson 1/Task2/Program.cs
@@ -1,8 +1,85 @@
-﻿int i1 = 5;
-int i2 = 6;
-int sum = i1 + i2;
-Console.WriteLine(sum);
+﻿// int i1 = 5;
+// int i2 = 6;
+// int sum = i1 + i2;
+// Console.WriteLine(sum);
 
-long  l1 =  6;
-long sum2 = l1+i1;
-Console.WriteLine(sum);
+// long  l1 =  6;
+// long sum2 = l1+i1;
+// Console.WriteLine(sum);
+
+class UserInputToCompileForTest
+{ 
+    // Разница между максимальным и минимальным элементами массива
+    
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
+    {
+        double min = numbers[0];
+        foreach (double item in numbers)
+        {
+            if(item < min){
+                min = item;
+            }
+        }
+        return min;
+    }
+    
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
+    {
+       double max = numbers[0];
+        foreach (double item in numbers)
+        {
+            if(item > max){
+                max = item;
+            }
+        }
+        return max;
+    }
+    
+    
+    
+    public static void PrintResult(double[] array)
+    { 
+        double min = array[0];
+        double max = array[0];
+        for (int i = 0; i < array.Length; i++)
+        {
+            if(array[i] < min){
+                min = array[i];
+            }
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+        }
+        Console.Write(max - min);
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        double[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(double.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new double[] {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
